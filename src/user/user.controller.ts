ea63ca883ba0b6b18f1c14883490f74b79d5de9d
sunhx <673686754@qq.com>
2024-02-27 16:47:28 +0800

    feat: register-user dto

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
+import { RegisterUserDto } from './dto/register-user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post('register')
+  register(@Body() registerUser: RegisterUserDto) {
+    console.log(registerUser);
+    return 'success';
+  }
 }
