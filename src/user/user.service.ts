4884b9861f530f078e130f3e2be00cc9b67644f0
sunhx <673686754@qq.com>
2024-02-27 20:58:41 +0800

    feat: user module

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { md5 } from 'src/utils';
+import { Repository } from 'typeorm';
+
+import { RegisterUserDto } from './dto/register-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private logger = new Logger();
+
+  @InjectRepository(User)
+  private userRepository: Repository<User>;
+
+  async register(user: RegisterUserDto) {}
+}
