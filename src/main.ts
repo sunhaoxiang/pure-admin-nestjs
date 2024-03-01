b01433ec810a9505a73a8b0fcfcaf5f607dd4f9c
sunhx <673686754@qq.com>
2024-03-01 14:53:20 +0800

    feat: add invoke-record interceptor

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from './app.module'
 import { FormatResponseInterceptor } from './format-response.interceptor'
+import { InvokeRecordInterceptor } from './invoke-record.interceptor'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.useGlobalPipes(new ValidationPipe())
   app.useGlobalInterceptors(new FormatResponseInterceptor())
+  app.useGlobalInterceptors(new InvokeRecordInterceptor())
 
   const configService = app.get(ConfigService)
   await app.listen(configService.get('NEST_SERVER_PORT'))
