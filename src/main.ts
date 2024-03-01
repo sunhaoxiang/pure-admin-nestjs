dd5f51389c8455fd8b75d71fba35726a5c5f366d
sunhx <673686754@qq.com>
2024-03-01 14:44:20 +0800

    feat: add format-response interceptor

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from './app.module'
+import { FormatResponseInterceptor } from './format-response.interceptor'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
   app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalInterceptors(new FormatResponseInterceptor())
+
   const configService = app.get(ConfigService)
   await app.listen(configService.get('NEST_SERVER_PORT'))
 }
