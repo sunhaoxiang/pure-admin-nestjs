029983db2c6aac19aa3b84b035bf9ddc9a092db6
sunhx <673686754@qq.com>
2024-03-26 16:59:16 +0800

    feat: seed

---
+++ prisma/seed.ts
@@ -0,0 +1,35 @@
+import * as crypto from 'crypto'
+
+import { Prisma, PrismaClient } from '@prisma/client'
+
+function md5(str: string) {
+  const hash = crypto.createHash('md5')
+  hash.update(str)
+  return hash.digest('hex')
+}
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const newUser: Prisma.UserCreateInput = {
+    username: 'admin',
+    password: md5('123456'),
+    email: 'xxx@xx.com',
+    nickName: '张三',
+    phoneNumber: '13233323333',
+    isAdmin: true
+  }
+  const user = await prisma.user.create({
+    data: newUser
+  })
+  console.log(user)
+}
+
+main()
+  .catch(e => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
