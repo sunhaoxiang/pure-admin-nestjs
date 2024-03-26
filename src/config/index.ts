9c9bfc67bc644da07077914187b541fa78174665
sunhx <673686754@qq.com>
2024-03-26 14:55:13 +0800

    feat: migrate from typeorm to prisma

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,6 @@
 import databaseConfig from './database.config'
-import dbConfig from './db.config'
 import jwtConfig from './jwt.config'
 import nodemailerConfig from './nodemailer.config'
 import redisConfig from './redis.config'
 
-export default [dbConfig, databaseConfig, redisConfig, nodemailerConfig, jwtConfig]
+export default [databaseConfig, redisConfig, nodemailerConfig, jwtConfig]
