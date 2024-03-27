d5b9e463617a1fc1237cb9d7bcee30d05fb87077
sunhx <673686754@qq.com>
2024-03-27 14:50:29 +0800

    feat: permission.guard

--- src/guards/permission.guard.ts
+++ src/guards/permission.guard.ts
@@ -33,7 +33,7 @@ export class PermissionGuard implements CanActivate {
 
     for (let i = 0; i < requiredPermissions.length; i++) {
       const curPermission = requiredPermissions[i]
-      const found = permissions.find(item => item.code === curPermission)
+      const found = permissions.find(item => item === curPermission)
       if (!found) {
         throw new UnauthorizedException('您没有访问该接口的权限')
       }
